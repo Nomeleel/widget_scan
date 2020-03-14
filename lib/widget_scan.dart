66da26e211ca100c05ca9a6aa1acbcfeb7297f96
Nomeleel <choonlay@outlook.com>
2020-03-14 22:26:52 +0800

    Design funcation.

--- lib/widget_scan.dart
+++ lib/widget_scan.dart
@@ -1,7 +1,18 @@
 library widget_scan;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+class WidgetScan {
+  List<WidgetObject> getWidgetList([String limitPath]) {
+    return List<WidgetObject>();
+  }
+
+  Map<String, WidgetBuilder> translateToRoutes(List<WidgetObject> widgetList) {
+    return Map<String, WidgetBuilder>();
+  }
+}
+
+class WidgetObject {
+  Widget widget;
+  String name;
 }
--- test/widget_scan_test.dart
+++ test/widget_scan_test.dart
@@ -3,11 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:widget_scan/widget_scan.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('to do', () {
+    final widgetScan = WidgetScan();
+    //ToDo
   });
 }
