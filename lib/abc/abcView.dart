fea109f672c4eee1c44834d2dd980039b71daac8
Nomeleel <choonlay@outlook.com>
2020-04-23 23:05:36 +0800

    Delete test file.

--- lib/abc/abcView.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class AbcView extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('abc');
-  }
-}
--- lib/def/defView.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class DefView extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('def');
-  }
-}
